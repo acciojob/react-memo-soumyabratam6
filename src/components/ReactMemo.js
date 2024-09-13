44316adb46bf65101c2232c916fddb2b93446940
soumyabrata <soumyabratam6@gmail.com>
2024-09-13 12:56:35 +0530

    all file edit and fix

--- src/components/ReactMemo.js
+++ src/components/ReactMemo.js
@@ -8,7 +8,7 @@ const ReactMemo = () => {
     <div>
         <h1>React.memo</h1>
         <input id='skill-input' type='text' onChange={(e)=>setSkillinput(e.target.value)}  value={skillinput}/>
-        <button id='skil-btn' onClick={()=>setSkills([...skills,skillinput])}>Add Skill</button>
+        <button id='skill-btn' onClick={()=>setSkills([...skills,skillinput])}>Add Skill</button>
         <ul id='item-jumbotron'>
             {
               skills.map((skill,index)=><li key={index}>{skill}</li>)
